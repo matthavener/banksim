91995746022096081c72b4c98b52ab5825d2bab5
Matt Havener <info@steepturnsoftware.com>
2012-05-07 20:45:44 -0500

    working well

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject banksim "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]])
--- src/banksim/core.clj
+++ src/banksim/core.clj
@@ -5,8 +5,10 @@
 (def grid-size 10)
 (def grid-size-pix 400)
 (def block-size-pix (/ grid-size-pix grid-size))
-(def truck-count 5)
+(def truck-count 3)
 (def bank-count 10)
+(def bank-decay-sleep-ms 1000)
+(def truck-move-sleep-ms 100)
 (def running true)
 
 (defn random-locations [s]
@@ -23,6 +25,8 @@
 
 (def central-bank-loc [(/ grid-size 2) (/ grid-size 2)])
 
+(def central-bank (cell-at central-bank-loc))
+
 (def bank-locs (filter #(not= central-bank-loc %) (random-locations bank-count)))
 
 (defn setup-world []
@@ -36,11 +40,11 @@
     (dosync (alter (cell-at central-bank-loc) assoc :bank 0))
   truck-locs))
 
-(defn bank-decay [_] 
-  (when running (send-off *agent* #'bank-decay))
-  (Thread/sleep 1000)
+(defn bank-decay [_]
+  (when running (send-off *agent* bank-decay))
+  (Thread/sleep bank-decay-sleep-ms)
   (doseq [b bank-locs]
-    (dosync (alter (cell-at b) update-in [:bank] / 2)))
+    (dosync (alter (cell-at b) update-in [:bank] quot 2)))
   (dosync (alter (cell-at central-bank-loc) update-in [:bank] + 10000)))
 
 (defn vector-to [l1 l2] (vec (map - l1 l2)))
@@ -62,41 +66,63 @@
                      {:loc l
                       :money ((cell-at l) :bank)
                       :dist (distance-sq orig l)}) bank-locs)
-        need-money (filter #(== (% :money) 0) banks)]
+        need-money (filter #(< (% :money) 25) banks)]
     (:loc (first (sort #(compare (%1 :dist) (%2 :dist)) need-money)))))
+
+(defn bound-num [n low high]
+  (max (min n high) low))
 
 (defn move-towards [from towards]
   (let [[x y] from
-        [dx dy] (normalize (vector-to from towards))
-        new-loc [(+ dx x) (+ dy y)]]
-    new-loc))
+        [dx dy] (normalize (vector-to towards from))
+        [xub yub] [(+ dx x) (+ dy y)]
+        ]
+    [(bound-num xub 0 grid-size) (bound-num yub 0 grid-size)]))
 
+(defn avail-for-truck [loc] (not (@(cell-at loc) :truck)))
+
+(defn drive-truck-towards [truck-loc towards]
+  (let [ideal (move-towards truck-loc towards)
+        random-move [(- (rand-int 3) 1) (- (rand-int 3) 1)]
+        random (move-towards truck-loc random-move)
+        next-loc (if (avail-for-truck ideal)
+                   ideal
+                   (if (avail-for-truck random)
+                     random
+                     truck-loc))
+        old-cell (cell-at truck-loc)
+        new-cell (cell-at next-loc)
+        money (@old-cell :truck)]
+;    (println "move " truck-loc " to " next-loc)
+    (alter old-cell dissoc :truck)
+    (alter new-cell assoc :truck money)
+    next-loc))
+
+(defn behave-truck [truck-loc]
+  (dosync   
+   (let [cell (cell-at truck-loc)
+         money (@cell :truck)
+         central-bank (cell-at central-bank-loc)]
+;     (println "truckate" truck-loc " money " money)  
+     (if (== money 0)
+       (when (adjacent? truck-loc central-bank-loc)
+         (alter central-bank update-in [:bank] - 100)
+         (alter cell assoc :truck 100))
+       (when-let [near-bank (nearest-bank-needing-money truck-loc)]
+         (when (adjacent? truck-loc near-bank)
+           (alter (cell-at near-bank) update-in [:bank] + money)
+           (alter cell assoc :truck 0))))
+     (if (== money 0)
+       (drive-truck-towards truck-loc central-bank-loc)
+       (if-let [near-bank (nearest-bank-needing-money truck-loc)]
+         (drive-truck-towards truck-loc near-bank)
+         truck-loc)))))
+  
 (defn truckate [truck-loc]
   (do
+    (Thread/sleep truck-move-sleep-ms)
     (when running (send-off *agent* truckate))
-    (Thread/sleep 50)
-    (println "truckate")
-    (let [cell (cell-at truck-loc)
-          money (cell :truck)
-          central-bank (cell-at central-bank-loc)]
-      (if (== money 0)
-        (when (adjacent? truck-loc central-bank-loc)
-          (dosync
-           (alter central-bank update-in [:bank] - 100)
-           (ref-set cell {:truck 100})))
-        (let [near-bank (nearest-bank-needing-money truck-loc)]
-          (when (adjacent? truck-loc near-bank)
-            (dosync
-             (alter (cell-at near-bank) update-in [:bank] + money)
-             (ref-set cell {:truck 0})))))
-      (let [new-loc
-            (if (== money 0)
-              (move-towards truck-loc central-bank-loc)
-              (move-towards truck-loc (nearest-bank-needing-money truck-loc)))]
-        (dosync (alter (cell-at new-loc) @cell
-        
-            
-       
+    (behave-truck truck-loc)))
 
 (defn color-for-cell [c]
   (cond
@@ -105,8 +131,9 @@
     :else Color/WHITE))
 
 (defn money-for-cell [c]
+
   (first (drop-while nil? (list (c :truck) (c :bank) 0))))
-    
+
 (defn paint-panel [g]
   (dosync 
    (doseq [x (range grid-size) y (range grid-size)]
@@ -142,7 +169,9 @@
                (paint-panel g)))))))
 
 (defn start []
-  (let [truck-locs (setup-world)]
+  (let [truck-locs (setup-world)
+        truck-agents (map #(agent %) truck-locs)]
     (send-off frame redraw-act)
-    (doseq [t truck-locs] (send-off (agent t) truckate))
-    (send-off (agent nil) bank-decay)))
+    (doseq [t truck-agents] (send-off t truckate))
+    (send-off (agent nil) bank-decay)
+    truck-agents))
